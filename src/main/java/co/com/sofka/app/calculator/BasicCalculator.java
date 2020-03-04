26af86250af5722977bbdacdde01b0d7e057079a
deivi ocampo <deibi.ocampo@sofka.com.co>
2020-03-04 11:41:16 -0500

    test de divicion por cero

--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -1,18 +1,22 @@
 package co.com.sofka.app.calculator;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
-
-import co.com.sofka.app.App;
-
-
+import java.sql.PreparedStatement;
 
 public class BasicCalculator {
 	private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
 	public Long sum(Long number1, Long number2) {
-	  // logger.info("Summing {} + {}", number1, number2 +"");
+	  logger.info("Summing {} + {}", number1, number2 +"");
 	    return number1 + number2;
+	}
+
+	public Long div(Long number1, Long number2)  {
+if (number2==0) {
+	throw new IllegalArgumentException();
+}
+		return number1/number2;
 	}
 }
--- src/test/java/co/com/sofka/app/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/BasicCalculatorTest.java
@@ -2,26 +2,78 @@ package co.com.sofka.app;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import co.com.sofka.app.calculator.BasicCalculator;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BasicCalculatorTest {
     private final BasicCalculator basicCalculator = new BasicCalculator();
-    @Test
-    @DisplayName("Testing sum: 1+1=2")
-    public void sum() {
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void sum(Long first, Long second, Long expectedResult) {
         // Arrange
         Long number1 = 1L;
         Long number2 = 1L;
-        Long expectedValue = 2L;
+        Long expectedValue = 20L;
         
         // Act
         Long result = basicCalculator.sum(number1, number2);
 
         // Assert
-        
-        assertEquals(expectedValue, result);
+
+        Assertions.assertEquals(expectedValue, result);
+
+
 }
+
+
+    @ParameterizedTest(name = "{0} / {1} = {2}")
+    @CsvSource({
+            "2,    1,   2",
+            "10,    5,   2",
+            "20,  10, 2",
+            "40,  20, 2"
+    })
+    @DisplayName("valido divicion")
+    public void divideValidation(Long first, Long second, Long expectedResult)
+{
+
+    Long expectedValue =basicCalculator.div(first,second);
+
+    Assertions.assertEquals(0,expectedResult);
+
 }
+
+    @ParameterizedTest(name = "{0} / {1}")
+    @CsvSource({
+            "0,    0, ",
+            "4,    0, "
+
+    })
+    @DisplayName("valido divicion por cero")
+    public void divideValidationByZero(Long first, Long second)
+    {
+
+
+        Assertions.assertThrows(IllegalAccessError.class,()->{
+                            basicCalculator.div(first,second);
+    });
+
+}
+
+    }
+
